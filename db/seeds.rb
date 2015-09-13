a4cb2fa53ea790ba0bdb14609d21653eb625e806
Kate Wood <katewood611@gmail.com>
2015-09-12 20:15:24 -0700

    adds navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Admin", email: "dndkpj@gmail.com", password: "crizza2015", password_confirmation: "crizza2015")
