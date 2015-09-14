d04409a02b9462bd06af09c8f4672f1ed905fc8b
Kate Wood <katewood611@gmail.com>
2015-09-14 01:16:54 -0700

    adds pages controller for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'pages/main'
+
   resources :inquiries
 
   get '/login'     => 'sessions#new'
   post '/login'    => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  get '/:id' => 'users#show', as: :user
+  
+
+  get 'users/:id' => 'users#show', as: :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
