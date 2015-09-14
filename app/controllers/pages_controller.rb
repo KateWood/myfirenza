b244146a2deb29a6056e3dc0b78484ebf1fd03f0
Kate Wood <katewood611@gmail.com>
2015-09-14 01:27:44 -0700

    reorganizes views to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
 class PagesController < ApplicationController
-  def main
-  end
+	def home
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'pages/main'
+  get 'pages/home', as: :home
 
   resources :inquiries
 
