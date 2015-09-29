bfcb4c30b9a3ec31a37c135da890e969fac9ee9f
Kate Wood <katewood611@gmail.com>
2015-09-29 14:34:20 -0700

    creates inquiry mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "no-reply@myfirenza.com"
+  layout 'mailer'
+end
---
+++ app/mailers/inquiry_mailer.rb
@@ -0,0 +1,10 @@
+class InquiryMailer < ApplicationMailer
+	default :from => "inquiry_alert@myfirenza.com"
+
+	def notification_email(inquiry)
+		@inquiry = inquiry
+		@url = "http://myfirenza.com/login"
+		mail(:to => "dave@firenzapizza.com", :subject => "You have received a new inquiry on myfirenza.com")
+		mail(:to => "dbaer@firenzapizza.com", :subject => "You have received a new inquiry on myfirenza.com")		
+	end
+end
